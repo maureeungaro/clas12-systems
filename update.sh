4fc079ab6d18a88a2b8e9c1b765287fabc40e463
maureeungaro <maureeungaro@gmail.com>
2022-04-08 10:45:10 -0400

    added update.sh and GeoArgParse.groovy

---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/bin/tcsh -f
+
+# Notice: no need to set COATJAVA env as the groovy script will pick up the relative location
+
+# Linux: -c = do not get file if already done.
+if (`uname` == "Linux") then
+	set mwget = "wget -c -nv --no-check-certificate"
+# Mac: makes it quite and show nice progress
+else if (`uname` == "Darwin") then
+	set mwget = "wget -qc --show-progress --no-check-certificate"
+endif
+
+rm -rf coat*jar jcsg*jar vecmath*jar
+
+set USEDEVEL = "no"
+
+set COATJAVA = 6.6.1
+set REPO     = https://clasweb.jlab.org/clas12offline/distribution/coatjava
+set COATFILE = coatjava-$COATJAVA".tar.gz"
+rm -rf clas12-offline-software
+
+
+# NOT DEVELOPMENT, DOWNOADING COATJAVA TAGGED RELEASE
+echo Dowloading $COATFILE version $COATJAVA":"  $REPO/$COATFILE
+
+$mwget  --trust-server-names $REPO/$COATFILE -O $COATFILE
+
+echo Unpacking $COATFILE
+
+tar -xf $COATFILE
+cp coatjava/lib/clas/* .
+
