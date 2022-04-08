4b5ca5a6c077114eaccee7aa212daf1f2172d1c7
maureeungaro <maureeungaro@gmail.com>
2022-04-08 15:39:37 -0400

    groovyFactories and systemsTxtDB

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 SCI-G and GEMC software to build CLAS12 systems 
 
+## To Run
+
+- to download coatjava:
+
+  `update.sh`
+
+- to run coatjava to create system volume parameters:
+
+  `cd groovyFactories` 
+  `runFactories.sh` systemName
+
 ## Includes 
 
 SCI-G scripts and digitization plugins for the CLAS12 detectors.
--- groovyFactories/runFactories.sh
+++ groovyFactories/runFactories.sh
@@ -1,29 +1,77 @@
 #!/bin/zsh
 
+# log eye candy
 green=`tput setaf 2`
 yellow=`tput setaf 3`
 
+# variation dictionary
 declare -A variations
-
+variations[bst]='default'
 variations[dc]='default'
 variations[ftof]='default rga_spring2018 rga_fall2018'
+variations[ec]='default rga_fall2018'
+variations[pcal]='default rga_spring2018 rga_fall2018'
 
-# Use the = parameter expansion specified to apply IFS word splitting.
-# For example $=foo splits at whitespace as determined by IFS.
+# available system
+availableSystems=(bst dc ftof ec pcal)
+
+
+# do not edit below this point
+txtDbOutputDir=../systemsTxtDB/
+systemsToRun=()
+availableLog="Available systems: 'all' or one of $availableSystems"
+
+if [ -z "$1" ]; then
+	echo
+	echo "No system supplied. "$availableLog
+	echo
+	exit
+fi
+
+matchedSystem () {
+	system=$1
+
+	for s in $availableSystems
+	do
+		if [[ $system == $s ]]; then
+			systemsToRun=$system
+			echo Systems set to: $systemsToRun
+			echo
+		fi
+	done
+
+	if [[ $system == "all" ]]; then
+		systemsToRun=("${availableSystems[@]}")
+		echo Systems set to: $green$systemsToRun$reset
+	fi
+	if [[ ${#systemsToRun[@]} == 0 ]]; then
+		echo
+		echo "System $system not available. "$availableLog
+		echo
+		exit
+	fi
+}
+
+# check that argument is 'all' or one of the available systems
+matchedSystem $1
 
 echo
-for det in dc ftof
+for det in $systemsToRun
 do
 	thisVariations=${variations[$det]}
 	for variation in $=thisVariations
 	do
-		echo $green$det$reset $yellow$variation$reset
-		groovy -cp "..:../*" $det".groovy" --variation $variation --runnumber 11
+		logFile=$det"__groovyLog_"$variation".txt"
+		echo $green$det$reset $yellow$variation$reset, log to $logFile
+		groovy -cp "..:../*" $det".groovy" --variation $variation --runnumber 11 > $logFile
 		toMove=$det"__volumes_$variation.txt"
-		[ -f $toMove ] && mv $toMove ../systems
+		[ -f $toMove ] && mv $toMove $txtDbOutputDir
 		toMove=$det"__parameters_$variation.txt"
-		[ -f $toMove ] && mv $toMove ../systems
+		[ -f $toMove ]  && mv $toMove $txtDbOutputDir
+		[ -f $logFile ] && mv $logFile $txtDbOutputDir
 	done
+	echo $green$det$reset done
+	echo
 done
 echo
 echo
