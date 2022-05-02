fdf49c0c4bc64c89677484a45330fae575915523
maureeungaro <maureeungaro@gmail.com>
2022-05-02 15:46:36 -0400

    added script to install libs

---
+++ installDependencyLibs.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+MYBUILDDIR=/Users/ungaro/builds
+CONFIGURATION_BUILD_DIR=$MYBUILDDIR
+
+# Purpose: install to libraries needed to run gemc + clas12 systems in $CONFIGURATION_BUILD_DIR
+
+#open $QTDIR/lib/
+#	open $ROOTSYS/lib/
+#	open $XERCESCROOT/lib/
+#	open $CCDB_HOME/lib/
+
+
+## XCODE
+
+cd ft/ft_cal/
+./ft_cal.py
+cd -
+
+cp $CLHEP_BASE_DIR/lib/libCLHEP-2.4.4.2.dylib $CONFIGURATION_BUILD_DIR
+cp $G4ROOT/$GEANT4_VERSION/lib/libG4*.dylib   $CONFIGURATION_BUILD_DIR
+cp $GLIBRARY/lib/*.dylib                      $CONFIGURATION_BUILD_DIR
+cp $XERCESCROOT/lib/libxerces-c-3.2.dylib     $CONFIGURATION_BUILD_DIR
+cp ft/ft_cal/ft_cal.jcard                     $CONFIGURATION_BUILD_DIR
+cp ft/ft_cal/ft_cal__geometry_default.txt     $CONFIGURATION_BUILD_DIR
+
+
+## GNU
+
+cd ft/ft_cal/plugin
+scons -j4 OPT=1
+cd -
+cp ft/ft_cal/plugin/ft_cal.gplugin $GPLUGIN_PATH
+cp $GLIBRARY/lib/gstreamer*        $GPLUGIN_PATH
