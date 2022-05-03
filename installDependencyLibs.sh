f7190139dfa4e83c15e882480669b3a6989d683e
Maurizio Ungaro <ungaro@jlab.org>
2022-05-03 10:40:10 -0400

    install ccdb in the script

--- installDependencyLibs.sh
+++ installDependencyLibs.sh
@@ -5,12 +5,6 @@ CONFIGURATION_BUILD_DIR=$MYBUILDDIR
 
 # Purpose: install to libraries needed to run gemc + clas12 systems in $CONFIGURATION_BUILD_DIR
 
-#open $QTDIR/lib/
-#	open $ROOTSYS/lib/
-#	open $XERCESCROOT/lib/
-#	open $CCDB_HOME/lib/
-
-
 ## XCODE
 
 cd ft/ft_cal/
@@ -18,12 +12,16 @@ cd ft/ft_cal/
 cd -
 
 cp $CLHEP_BASE_DIR/lib/libCLHEP-2.4.4.2.dylib $CONFIGURATION_BUILD_DIR
+cp $XERCESCROOT/lib/libxerces-c-3.2.dylib     $CONFIGURATION_BUILD_DIR
 cp $G4ROOT/$GEANT4_VERSION/lib/libG4*.dylib   $CONFIGURATION_BUILD_DIR
 cp $GLIBRARY/lib/*.dylib                      $CONFIGURATION_BUILD_DIR
-cp $XERCESCROOT/lib/libxerces-c-3.2.dylib     $CONFIGURATION_BUILD_DIR
+cp $CCDB_HOME/lib/*.dylib                     $CONFIGURATION_BUILD_DIR
 cp ft/ft_cal/ft_cal.jcard                     $CONFIGURATION_BUILD_DIR
 cp ft/ft_cal/ft_cal__geometry_default.txt     $CONFIGURATION_BUILD_DIR
 
+echo
+echo Remember to Ccompile the ft_cal and the streamers plugins from XCode
+echo
 
 ## GNU
 
