76a48d0b0b48ec640b06fbe888e1c2d2fcc379e2
Maria <zurek@anl.gov>
2022-05-09 19:53:25 -0500

    Build ftof from volume files in the DB directory

--- ftof/ftof.py
+++ ftof/ftof.py
@@ -13,31 +13,10 @@ from materials import define_materials
 
 _logger = logging.getLogger("ftof")
 
-
 VARIATION_MAP = {
     "default": "ftof__volumes_default.txt",
     "rga_fall2018": "ftof__volumes_rga_fall2018.txt",
 }
-
-
-def generate_volumes(variation: str, run_number: int, exe: str = "groovy", **kwargs):
-    args = [
-        str(exe),
-        "-cp",
-        "../*:..",
-        "factory.groovy",
-        "--variation", str(variation),
-        "--runnumber", str(run_number),
-    ]
-    _logger.debug(f"Calling subprocess with args: {args}")
-    try:
-        subprocess.run(args, **kwargs)
-    except FileNotFoundError:
-        _logger.exception(f"Executable {exe} not found")
-    except subprocess.SubprocessError as e:
-        # TODO add exception handling
-        _logger.exception("Error running Groovy")
-
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
@@ -48,8 +27,8 @@ def main():
 
     # loop over all the defined builder functions
     for var_key, file_name in VARIATION_MAP.items():
-        _logger.info(f"Generating volume files for variation {var_key}")
-        generate_volumes(var_key, run_number=11)
+
+        basepath = os.environ["GPLUGIN_PATH"]
 
         _logger.info(f"Building FTOF configuration for variation {var_key}")
         # Define GConfiguration name, factory and description. Initialize it.
@@ -62,7 +41,7 @@ def main():
         define_materials(configuration)
 
         # run geometry file bulder for selected variation
-        apply_configuration(file_name, configuration)
+        apply_configuration(f'{basepath}/{file_name}', configuration)
         # print out the GConfiguration
         configuration.printC()
 
