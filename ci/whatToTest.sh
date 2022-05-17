6838e4168b9120dbbcfdcbee5e0170b81417fab0
Maurizio Ungaro <ungaro@jlab.org>
2022-05-17 10:22:25 -0400

    test3

--- ci/whatToTest.sh
+++ ci/whatToTest.sh
@@ -97,7 +97,7 @@ else # Push
 	FILESCHANGED=$( git diff --name-only $GITHUB_BEFORE $GITHUB_SHA | grep -v .github/workflows )
 fi
 
-systemsChanged=(a b c )
+systemsChanged=()
 for f in $FILESCHANGED
 do
 	bdir=$(dirname $f)
