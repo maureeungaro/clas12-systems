aecfc68033d03b14a995f4514466db014dba69a8
maureeungaro <maureeungaro@gmail.com>
2022-05-18 18:00:31 -0400

    emptying TT routin for ftof

--- ftof/plugin/loadTT.cc
+++ ftof/plugin/loadTT.cc
@@ -14,32 +14,6 @@ vector<vector<double> > data;
 
 bool FTOF_Plugin::loadTT(int runno, string variation)
 {
-	translationTable = new GTranslationTable();
-
-	vector<vector<double> > data;
-
-	string database   = "/daq/tt/ftcal:1";
-	gLogMessage("FT-CAL: Loading Translation Table " + database);
-	data.clear(); calib->GetCalib(data, database);
-
-	// filling translation table
-	for(unsigned row = 0; row < data.size(); row++) {
-		int crate   = data[row][0];
-		int slot    = data[row][1];
-		int channel = data[row][2];
-
-		int crystal = data[row][5];
-
-		// mode 0: "crate" is the frame source
-		// mode 1: "crate/slot" is the frame source
-		// mode 2: "crate/slot/channel" is the frame source
-		int mode = 0;
-
-		int iX = crystal%22 + 1;
-		int iY = crystal/22 + 1;
-
-		translationTable->addGElectronicWithIdentity({iX, iY}, GElectronic(crate, slot, channel, mode));
-	}
 
 	return true;
 }
