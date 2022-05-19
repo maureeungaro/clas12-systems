c152a5046d67644f9e761f47f3304bba485adb19
maureeungaro <maureeungaro@gmail.com>
2022-05-19 14:06:30 -0400

    adding content to ftof loading TT

--- ftof/plugin/loadTT.cc
+++ ftof/plugin/loadTT.cc
@@ -14,6 +14,29 @@ vector<vector<double> > data;
 
 bool FTOF_Plugin::loadTT(int runno, string variation)
 {
+	translationTable = new GTranslationTable();
+
+	vector<vector<double> > data;
+
+	string database   = "/daq/tt/ftof:1";
+	gLogMessage("FTOF: Loading Translation Table " + database);
+	data.clear(); calib->GetCalib(data, database);
+
+	// filling translation table
+	for(unsigned row = 0; row < data.size(); row++) {
+		int sector = data[row][3];
+		int panel = data[row][4];
+		int paddle = data[row][5];
+		int pmt = data[row][6];
+
+		// mode 0: "crate" is the frame source
+		// mode 1: "crate/slot" is the frame source
+		// mode 2: "crate/slot/channel" is the frame source
+		int mode = 0;
+
+		// order is important as we could have duplicate entries w/o it
+		translationTable->addGElectronicWithIdentity({sector, panel, paddle, pmt}, GElectronic(crate, slot, channel, mode));
+	}
 
 	return true;
 }
