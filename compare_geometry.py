dc5fb113628ef49837416666d41ad2d8582b746b
Maria <zurek@anl.gov>
2022-05-19 20:25:54 -0500

    Add exit code

--- compare_geometry.py
+++ compare_geometry.py
@@ -5,15 +5,26 @@ __author__ = "Maria K Zurek <zurek@anl.gov>"
 
 import argparse
 from dataclasses import dataclass, field
+from enum import IntEnum
 import logging
 import math
 import os
 from pprint import pprint
 import sys
-from typing import List, Iterable, Dict, Callable, Tuple
+from typing import List, Iterable, Dict, Callable, Tuple, Optional
 
 
 _logger = logging.getLogger("compare_geometry")
+
+
+
+class ExitCode(IntEnum):
+    OK = 0
+    FAILED = 1
+    ERROR = 2
+
+
+FAILURES = []
 
 
 @dataclass
@@ -373,8 +384,9 @@ def get_indexed_volumes(items: Iterable[VolumeParams]) -> Dict[str, VolumeParams
 def compare_indexed_volumes(
         gemc2_volumes: Iterable[VolumeParams],
         gemc3_volumes: Iterable[VolumeParams],
-        matchers: Iterable[Callable]
-    ) -> Dict[str, Dict[Callable, MatcherResult]]:
+        matchers: Iterable[Callable],
+        file_info: Optional[str] = "",
+    ) -> Dict[str, Dict[str, MatcherResult]]:
 
     all_results = {}
     for volume_id, gemc2_vol in gemc2_volumes.items():
@@ -392,11 +404,13 @@ def compare_indexed_volumes(
         all_results[volume_id] = match_results
         for matcher, match_res in match_results.items():
             if not match_res.is_equal:
-                _logger.warning(f"For volume {volume_id}, matcher {matcher}: {match_res}")
+                message = f"For volume {volume_id}, matcher {matcher}: {match_res}"
+                _logger.warning(message)
+                FAILURES.append((file_info, message))
     return all_results
 
 
-def compare_files_gemc2_gemc3(gemc2: os.PathLike, gemc3: os.PathLike) -> Dict[str, Dict[Callable, MatcherResult]]:
+def compare_files_gemc2_gemc3(gemc2: os.PathLike, gemc3: os.PathLike) -> Dict[str, Dict[str, MatcherResult]]:
     gemc2_vols = read_file(gemc2, "gemc2")
     gemc3_vols = read_file(gemc3, "gemc3")
 
@@ -430,7 +444,8 @@ def compare_files_gemc2_gemc3(gemc2: os.PathLike, gemc3: os.PathLike) -> Dict[st
     results = compare_indexed_volumes(
         get_indexed_volumes(gemc2_vols),
         get_indexed_volumes(gemc3_vols),
-        simple_equal_matchers + simple_is_close_matchers + simple_no_na_matchers + advanced_matchers
+        simple_equal_matchers + simple_is_close_matchers + simple_no_na_matchers + advanced_matchers,
+        file_info=f"{gemc2} -> {gemc3}"
     )
     return results
 
@@ -520,7 +535,7 @@ def _create_argument_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def main():
+def main() -> ExitCode:
     logging.basicConfig(level=logging.DEBUG)
 
     parser = _create_argument_parser()
@@ -540,8 +555,13 @@ def main():
             gemc3_file,
         )
         #pprint(single_file_pair_results)
+    if FAILURES:
+        _logger.error("The following matcher failures were detected:")
+        _logger.error("\n".join(FAILURES))
+        return ExitCode.FAILED
+    return ExitCode.OK
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
 
